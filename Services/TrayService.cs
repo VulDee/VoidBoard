aeee6b84cbb779c7d2f91c3c349b97949527d08d
VulDee <114196445+VulDee@users.noreply.github.com>
2026-05-12 16:42:45 +0800

    Update TrayService.cs

--- Services/TrayService.cs
+++ Services/TrayService.cs
@@ -1,6 +1,30 @@
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows;
+
 namespace VoidBoard.Services;
 
 public class TrayService
 {
-    // tray icon stuff later
+    private TaskbarIcon? trayIcon;
+
+    // tray icon
+
+    public void Setup(Window window)
+    {
+        trayIcon = new TaskbarIcon();
+
+        trayIcon.ToolTipText = "VoidBoard";
+
+        trayIcon.TrayLeftMouseDown += (s, e) =>
+        {
+            window.Show();
+            window.WindowState = WindowState.Normal;
+            window.Activate();
+        };
+    }
+
+    public void Dispose()
+    {
+        trayIcon?.Dispose();
+    }
 }
