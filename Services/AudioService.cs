d77338e0b515b3ba7733a66c56a1caff52c5b215
VulDee <114196445+VulDee@users.noreply.github.com>
2026-05-12 11:44:51 +0800

    Create AudioService.cs

---
+++ Services/AudioService.cs
@@ -0,0 +1,16 @@
+namespace VoidBoard.Services;
+
+public class AudioService
+{
+    // handles playback later
+
+    public void Play(string filePath, float volume)
+    {
+
+    }
+
+    public void StopAll()
+    {
+
+    }
+}
