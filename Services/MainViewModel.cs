5aad5f8d4c4d3e3772799c6cffb64f89aa80b14d
VulDee <114196445+VulDee@users.noreply.github.com>
2026-05-12 16:43:00 +0800

    Update MainViewModel.cs

--- Services/MainViewModel.cs
+++ Services/MainViewModel.cs
@@ -1,6 +1,24 @@
+using System.Collections.ObjectModel;
+using VoidBoard.Models;
+
 namespace VoidBoard.ViewModels;
 
 public class MainViewModel
 {
+    public ObservableCollection<SoundSlot> Slots { get; set; } = [];
 
+    public SettingsModel Settings { get; set; } = new();
+
+    public MainViewModel()
+    {
+        // makes 10 slots
+
+        for (int i = 0; i < 10; i++)
+        {
+            Slots.Add(new SoundSlot
+            {
+                SlotNumber = i
+            });
+        }
+    }
 }
