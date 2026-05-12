d18a3676d3cc067d51903e7fdfbe84fadc77de9b
VulDee <114196445+VulDee@users.noreply.github.com>
2026-05-12 16:42:30 +0800

    Update HotkeyService.cs

--- Services/HotkeyService.cs
+++ Services/HotkeyService.cs
@@ -1,6 +1,41 @@
+using NHotkey;
+using NHotkey.Wpf;
+
 namespace VoidBoard.Services;
 
 public class HotkeyService
 {
-    // global hotkeys later
+    // registers capslock + num hotkeys later
+
+    public void RegisterHotkey(
+        string name,
+        System.Windows.Input.Key key,
+        EventHandler<HotkeyEventArgs> action)
+    {
+        try
+        {
+            HotkeyManager.Current.AddOrReplace(
+                name,
+                key,
+                System.Windows.Input.ModifierKeys.None,
+                action
+            );
+        }
+        catch
+        {
+
+        }
+    }
+
+    public void RemoveHotkey(string name)
+    {
+        try
+        {
+            HotkeyManager.Current.Remove(name);
+        }
+        catch
+        {
+
+        }
+    }
 }
